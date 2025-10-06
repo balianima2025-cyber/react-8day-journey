f38a7ad19e24fb567f3a1dda9b866ff66222557a
balianima2025-cyber <balianima2025@gmail.com>
2025-10-06 08:32:01 +0800

    fix: useFetch infinite loop

--- src/day8/useFetch.js
+++ src/day8/useFetch.js
@@ -29,7 +29,7 @@ const useFetch = (url, options = {}) => {
     if (url) {
       fetchData();
     }
-  },  [url, options]); 
+  }, [url]); // Re-fetch when URL changes
 
   return { data, loading, error, refetch: () => {} };
 };
